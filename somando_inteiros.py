b39976a6726d7b5e60bd97963bd6cb1bb61f862f
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-09-26 20:18:05 -0300

    Add files via upload

---
+++ somando_inteiros.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+
+a =  int(input('Digite um número inteiro: '))
+b =  int(input('Digite outro número: '))
+
+print(a + b)
